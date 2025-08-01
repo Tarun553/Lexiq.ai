88aba24a3d409d48064cda6994bdff1d3c620884
tarun07 <tarunchoudhary553@gmail.com>
2025-08-01 12:59:11 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ await connectCloudinary();
 
 // CORS configuration
 const allowedOrigins = [
-  "http://localhost:5173", // Development
+  "https://lexiq-ai-1.onrender.com/", // Development
   process.env.FRONTEND_URL, // Production
 ].filter(Boolean); // Remove any undefined values
 
